4c2643c9ceda582ade874d141af780f121d405f9
hmtiannan <424065546@qq.com>
2020-04-13 11:11:29 +0800

    Update getnews.js

--- message/getnews.js
+++ message/getnews.js
@@ -1,6 +1,6 @@
 const axios = require('axios')
 
-const api = 'http://172.19.237.198:5000/news'
+const api = 'http://192.168.1.119:5000/news'
 
 function getTodayNews () {
   return axios.get(api).then(({ data }) => {
